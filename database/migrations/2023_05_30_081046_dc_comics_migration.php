4ba2adbaec8927a88bbb23312526a6c66a226a29
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-05-30 10:51:57 +0200

    feat: :sparkles: Adds: better menagment of controller's table. Blade.php files created and correclty linked

--- database/migrations/2023_05_30_081046_dc_comics_migration.php
+++ database/migrations/2023_05_30_081046_dc_comics_migration.php
@@ -14,14 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('dccomics', function (Blueprint $table){
-            $table->bigIncrements('id');
+            $table->id();
             $table->string('title');
-            $table->string('description');
-            $table->string('thumb');
-            $table->string('price');
-            $table->string('series');
-            $table->string('sale_date');
-            $table->string('type');
+            $table->text('description')->nullable();
+            $table->text('thumb');
+            $table->string('price', 10);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 30);
             $table->timestamps();
         });
     }
